d87ee8edecc3a49a1452429eaf686667e65ea348
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-13 14:35:39 +0700

    Send Location and Input, Button State

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,53 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input') 
+const $messageFormButton = $messageForm.querySelector('button') 
+
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    // disable
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+    
+    socket.emit('sendMessage', message, (error) => {
+
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        // enable
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message delivered')
+    })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolacation is not supported by your browser.')    
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -17,11 +18,27 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome to the chat app')
+    socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+
+        const fileter = new Filter()
+        if (fileter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+
         io.emit('message', message)
+        callback()
     })
 
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude}.${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
 })
 
 server.listen(port, () => {
